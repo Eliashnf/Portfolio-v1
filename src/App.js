5cc5a5c682d5d16807acbae4f4b5848ac66d63a0
Elias Hanafi <eliashnf@gmail.com>
2022-01-23 16:51:15 +0100

    Switch -> Routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import Accueil from './pages/Accueil';
 import NotFound from './pages/NotFound';
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route path="/" exact component={Accueil} />
-        <Route component={NotFound} />
-      </Switch>
+      <Routes>
+        <Route path="/" exact element={<Accueil/>} />
+        <Route path="*" element={<NotFound/>} />
+      </Routes>
     </BrowserRouter>
   );
 };
